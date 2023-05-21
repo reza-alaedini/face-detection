665c38ea796cb882b75afe48e99ccae7f0a04466
Reza Alaedini <parsa.alaei.pa@gmail.com>
2023-05-21 19:36:28 +0330

    implement hole proj

---
+++ README.md
@@ -0,0 +1,35 @@
+# FaceMash Application
+
+See the full tutorial of this source code at the following address:
+https://www.youtube.com/watch?v=pNQ8wOYG3SA
+
+## Get in touch with me on: 
+
+website:
+http://neonlearn.ir
+
+telegram:
+https://t.me/neonlearnir
+
+instagram:
+https://www.instagram.com/m_h_sw/
+
+youtube:
+https://youtube.com/neonlearn
+
+### `npm start`
+
+Runs the app in the development mode.
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.\
+You will also see any lint errors in the console.
+
+### `npm run build`
+
+Builds the app for production to the `build` folder.\
+It correctly bundles React in production mode and optimizes the build for the best performance.
+
+The build is minified and the filenames include the hashes.\
+Your app is ready to be deployed!
+
---
+++ src/App.js
@@ -0,0 +1,83 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useRef, useState } from "react";
+import * as tf from "@tensorflow/tfjs";
+import * as faceLandmarksDetection from "@tensorflow-models/face-landmarks-detection";
+import Webcam from "react-webcam";
+import { drawMesh } from "./utilities";
+
+function App() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+  const [checked, setChecked] = useState(false);
+
+  const runFacemesh = async () => {
+    const net = await faceLandmarksDetection.load(
+      faceLandmarksDetection.SupportedPackages.mediapipeFacemesh
+    );
+
+    setInterval(() => {
+      detect(net);
+    }, 300);
+  };
+
+  const detect = async (net) => {
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      const video = webcamRef.current.video;
+      const videoWidth = video.videoWidth;
+      const videoHeight = video.videoHeight;
+
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+
+      const face = await net.estimateFaces({ input: video });
+      const ctx = canvasRef.current.getContext("2d");
+      drawMesh(face, ctx);
+    }
+  };
+
+  runFacemesh();
+  return (
+    <>
+      <div style={{ textAlign: "center", overflowX: "hidden" }}>
+        <h4>Reza Alaedini - IT Student - Shamsipour University</h4>
+      </div>
+      <div className="App">
+        <img src={logo} className="App-logo" />
+        <Webcam
+          ref={webcamRef}
+          className="webcamStyle"
+          style={{ filter: checked ? "grayscale(100%)" : "grayscale(0)" }}
+        />
+        <canvas
+          ref={canvasRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            width: 640,
+            height: 400,
+          }}
+        />
+        <br />
+        <div style={{ marginTop: 20 }}>
+          <label>Are You in Bright place?</label>
+          <input
+            type="checkbox"
+            checked={checked}
+            onChange={() => setChecked(!checked)}
+          />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
